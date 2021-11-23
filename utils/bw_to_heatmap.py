267525423359ffb8c2e3f082dc4785871fb8f463
MGui <ming.gui@tum.de>
2021-11-23 17:12:29 +0000

    Reinforced reinforce images

--- utils/bw_to_heatmap.py
+++ utils/bw_to_heatmap.py
@@ -4,18 +4,20 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+from matplotlib.colors import LinearSegmentedColormap
 
 result_path = '/root/Documents/BNN_KFAC/results/Hessian/images/'
-img_name = 'error_15k_foot.png'
+img_name = 'H_750_dense.png'
 file_path = result_path + img_name
 
+cmap = LinearSegmentedColormap.from_list('', [(0,'k'),(0.1,'purple'),(0.2,'b'),(0.3,'cyan'),(0.4,'g'),(0.5,'yellow'),(1,'salmon')])
 im = imageio.imread(file_path)
 i = torch.FloatTensor(im) / 255
 print(i.max())
-plt.figure(figsize = (12,12))
+plt.figure(figsize = (12,8))
 
 # plot = sns.heatmap(i[:,:,0], xticklabels=False, yticklabels=False, square=True, cbar=False)
-plot = sns.heatmap(i[:,:,0], vmin=0, vmax=0.6, xticklabels=False, yticklabels=False, square=True, cbar=False)
+plot = sns.heatmap(i[:,:,0], vmin=0, vmax=1, xticklabels=False, yticklabels=False, square=True, cbar=0, cmap=cmap)
 
 # plot.figure.savefig(result_path + 'heatmap/' + img_name)
 # plt.savefig(result_path + 'heatmap/' + img_name)
